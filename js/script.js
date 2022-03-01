880758d9184d6ca0b5f285877d8755c8d212aeb6
Mike B <mikeabarous@gmail.com>
2022-02-28 22:12:55 -0500

    Update script.js
    Created global variables

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//guessed letters
+const guessedLetters = document.querySelector(".guessed-letters");
+//guess button
+const guessButton = document.querySelector(".guess");
+//input form
+const inputGuess = document.querySelector(".guess-form");
+//guessed word progress paragraph
+const wordProgress = document.querySelector(".word-in-progress");
+//guesses remaing paragraph
+const remaining = document.querySelector(".remaining");
+//remaing guesses span
+const span = document.querySelector("span");
+//messages paragraph
+const message = document.querySelector(".message");
+//hidden play again button
+const playAgain = document.querySelector(".play-again");
+//test word
+const word = "magnolia";
